7a9c4fc3fef66a335d910de73f9a36faeaa35aa8
imliterallydanishlol <102416852+bambitheone82112@users.noreply.github.com>
2022-06-24 16:03:44 +0700

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,10 +81,10 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
-			['Psych Engine Team'],
-			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',							'https://twitter.com/Shadow_Mario_',	'444444'],
-			['RiverOaken',			'river',			'Main Artist/Animator of Psych Engine',						'https://twitter.com/RiverOaken',		'B42F71'],
-			['shubs',				'shubs',			'Additional Programmer of Psych Engine',					'https://twitter.com/yoshubs',			'5E99DF'],
+			['Dimensional engine Team'],
+			['bambitheone82112',		'ibmab',		'Main programmer of this engine',							'https://twitter.com/Shadow_Mario_',	'444444'],
+			['anonymous',			'forgot it',			'Main Artist/Animator of this engine',						'https://twitter.com/RiverOaken',		'B42F71'],
+			['slxickey',				'lmao',			'Additional Programmer of this engine',		
 			[''],
 			['Former Engine Members'],
 			['bb-panzu',			'bb',				'Ex-Programmer of Psych Engine',							'https://twitter.com/bbsub3',			'3E813A'],
